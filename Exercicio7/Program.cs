9833e84446330a301fb6b478acb2f582c6864a96
Wesley Nóbrega <wesley@dev>
2026-05-09 22:35:25 -0300

    Exercicio 7 e 8...

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,38 @@
+﻿using Exercicio6;
+
+namespace Exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+
+            Action<string> logMulticast = null;
+
+            Console.WriteLine("Delegate vazio...");
+            logMulticast?.Invoke("\nMensagem vazia");
+
+            Console.WriteLine("NÃO TIVEMOS EXCEÇÃO.");
+
+            Console.WriteLine("\nAdicionando métodos...");
+
+            logMulticast += logger.LogToConsole;
+            logMulticast += logger.LogToFile;
+
+            logMulticast?.Invoke("Mensagem de log registrada.");
+
+            Console.WriteLine("\nRemovendo todos os métodos dinamicamente:");
+
+            logMulticast -= logger.LogToConsole;
+            logMulticast -= logger.LogToFile;
+
+            logMulticast?.Invoke("Mensagem vazia");
+
+            Console.WriteLine("O sistema se mantém após a remoção e com os delegates nulos.");
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio8/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Exercicio8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exercicio8.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Produto> Produtos { get; set; }
+
+        public void OnGet()
+        {
+            Produtos = new List<Produto>
+            {
+                new Produto { Nome = "Arroz", Preco = 7.50m },
+                new Produto { Nome = "Feijão", Preco = 12.00m },
+                new Produto { Nome = "Macarrão", Preco = 8.99m }
+            };
+        }
+    }
+}
